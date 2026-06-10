9153942638886a77e8406cdabff69348774648b3
Harish K <kpharish2001@gmail.com>
2026-06-10 18:10:42 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,7 +95,6 @@ subprojects {
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.16")
         implementation("org.jsoup:jsoup:1.22.1")
-        implementation("androidx.annotation:annotation:1.9.1")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
         implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
