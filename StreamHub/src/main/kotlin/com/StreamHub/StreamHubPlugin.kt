abb81a593bcd53aa26c4defc93ba5039d929d35e
Harish K <kpharish2001@gmail.com>
2026-06-16 00:44:22 +0530

    Create StreamHubPlugin.kt

---
+++ StreamHub/src/main/kotlin/com/StreamHub/StreamHubPlugin.kt
@@ -0,0 +1,12 @@
+package com.streamhub
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class StreamHubPlugin : Plugin() { 
+    override fun load(context: Context) {
+        registerMainAPI(StreamHub())
+    }
+}
