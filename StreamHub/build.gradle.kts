cfe0383e4ab39f15428203a97c7202f114e21cfd
Harish K <kpharish2001@gmail.com>
2026-06-18 23:29:59 +0530

    Update build.gradle.kts

--- StreamHub/build.gradle.kts
+++ StreamHub/build.gradle.kts
@@ -1,11 +1,11 @@
 // use an integer for version numbers
-version = 1
+version = 6
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "111movies"
-    language = "en"
+    description = "Multi Server Provider"
+    language = "ta"
     authors = listOf("ChocoCooper")
 
     /**
