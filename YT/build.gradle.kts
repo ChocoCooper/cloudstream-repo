d5d964282687754140c20a9ecc44d1489164a96e
Harish K <kpharish2001@gmail.com>
2026-06-23 14:09:28 +0530

    Update and rename build.gradle.kts to build.gradle.kts

--- YT/build.gradle.kts
+++ YT/build.gradle.kts
@@ -1,14 +1,8 @@
-plugins {
-    id("com.android.library")
-    kotlin("android")
-    id("com.lagradost.cloudstream3.plugin")
-}
-
-// Use an integer for version numbers
+// use an integer for version numbers
 version = 2
 
 cloudstream {
-    name = "YouTube Plugin"
+    name = "YT"
     description = "Watch Youtube in Cloudstream"
     language = "en" 
     authors = listOf("ChocoCooper")
@@ -19,12 +13,4 @@ cloudstream {
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/3840px-YouTube_full-color_icon_%282017%29.svg.png"
 
     isCrossPlatform = true
-}
-
-dependencies {
-    val cloudstream by configurations
-    val compileOnly by configurations
-
-    cloudstream("com.lagradost:cloudstream3:pre-release")
-    compileOnly("com.github.TeamNewPipe:NewPipeExtractor:v0.24.2")
 }
