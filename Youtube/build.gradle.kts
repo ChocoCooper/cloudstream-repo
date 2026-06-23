d2b8e72b5c34c637280fbe55787c2d6893d8a82f
Harish K <kpharish2001@gmail.com>
2026-06-23 14:01:19 +0530

    Update build.gradle.kts

--- Youtube/build.gradle.kts
+++ Youtube/build.gradle.kts
@@ -1,16 +1,30 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.lagradost.cloudstream3.plugin")
+}
+
 // Use an integer for version numbers
 version = 2
 
 cloudstream {
+    name = "YouTube Plugin"
     description = "Watch Youtube in Cloudstream"
-    language = "en" // FIX: Matched to English so it shows up properly in the app
+    language = "en" 
     authors = listOf("ChocoCooper")
 
     status = 1 
 
-    // FIX: Changed "Other" to "Others" to match Cloudstream's TvType exactly
     tvTypes = listOf("Others", "Live", "TvSeries")
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/3840px-YouTube_full-color_icon_%282017%29.svg.png"
 
     isCrossPlatform = true
+}
+
+dependencies {
+    val cloudstream by configurations
+    val compileOnly by configurations
+
+    cloudstream("com.lagradost:cloudstream3:pre-release")
+    compileOnly("com.github.TeamNewPipe:NewPipeExtractor:v0.24.2")
 }
