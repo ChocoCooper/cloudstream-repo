ae833998fdecc2e4d459a4e5feb1736d1c4c4eaa
Harish K <kpharish2001@gmail.com>
2026-07-05 01:39:43 +0530

    Create AnimeJokerPlugin.kt

---
+++ AnimeJoker/src/main/kotlin/com/AnimeJoker/AnimeJokerPlugin.kt
@@ -0,0 +1,11 @@
+﻿package com.AnimeJoker
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class AnimeJokerPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI((AnimeJokerProvider))
+    }
+}
