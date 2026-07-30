660ef39809597a97e776ce3eba725c2b6f877e93
Harish K <kpharish2001@gmail.com>
2026-07-30 16:27:23 +0530

    Update Happy2hubProvider.kt

--- Happy2hub/src/main/kotlin/com/Happy2hub/Happy2hubProvider.kt
+++ Happy2hub/src/main/kotlin/com/Happy2hub/Happy2hubProvider.kt
@@ -5,12 +5,16 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 import com.lagradost.cloudstream3.extractors.PixelDrain
 import com.lagradost.cloudstream3.extractors.Voe
+import com.lagradost.cloudstream3.extractors.DoodLaExtractor
+import com.lagradost.cloudstream3.extractors.LuluStream
 
 @CloudstreamPlugin
-class Happy2hubProvider: Plugin() {
+class Happy2hubProvider : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(Happy2hub())
         registerExtractorAPI(Voe())
         registerExtractorAPI(PixelDrain())
+        registerExtractorAPI(DoodLaExtractor())
+        registerExtractorAPI(LuluStream())
     }
 }
