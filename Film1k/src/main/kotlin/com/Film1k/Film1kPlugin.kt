4b494cd74c808cd834acb4e557dd0214d6e6c9cb
Harish K <kpharish2001@gmail.com>
2026-07-31 23:32:38 +0530

    Update Film1kPlugin.kt

--- Film1k/src/main/kotlin/com/Film1k/Film1kPlugin.kt
+++ Film1k/src/main/kotlin/com/Film1k/Film1kPlugin.kt
@@ -1,11 +1,8 @@
 package com.film1k
 
 import android.content.Context
-import com.lagradost.cloudstream3.MainActivity
-import com.lagradost.cloudstream3.CloudStreamApp.Companion.getKey
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import kotlinx.coroutines.runBlocking
 
 @CloudstreamPlugin
 class Film1kPlugin : Plugin() {
