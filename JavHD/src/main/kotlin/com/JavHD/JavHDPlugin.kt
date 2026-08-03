b0fd413e27ed36e1a7665224bb435e06c6985e59
Harish K <kpharish2001@gmail.com>
2026-08-04 02:05:06 +0530

    Create JavHDPlugin.kt

---
+++ JavHD/src/main/kotlin/com/JavHD/JavHDPlugin.kt
@@ -0,0 +1,20 @@
+package com.JavHD
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.StreamTape
+
+@CloudstreamPlugin
+class JAVHDPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(JAVHDProvider())
+        registerExtractorAPI(StreamTape())
+        registerExtractorAPI(Stbturbo())
+        registerExtractorAPI(Turbovid())
+        registerExtractorAPI(MyCloudZ())
+        registerExtractorAPI(Cloudwish())
+        registerExtractorAPI(TurbovidVip())
+        registerExtractorAPI(Streambeast())
+    }
+}
