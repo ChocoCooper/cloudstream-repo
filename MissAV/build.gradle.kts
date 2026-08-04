e0b311c9e1b17ada07649d1c6f02e7b3a675e0ff
Harish K <kpharish2001@gmail.com>
2026-08-04 11:10:34 +0530

    Create build.gradle.kts

---
+++ MissAV/build.gradle.kts
@@ -0,0 +1,22 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Browse JAV content via MissAV and 123AV"
+    authors = listOf("ChocoCooper")
+
+    /**
+     * Status int as one of the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+
+    tvTypes = listOf("NSFW")
+
+    iconUrl = "https://missav.ws/favicon.ico"
+}
