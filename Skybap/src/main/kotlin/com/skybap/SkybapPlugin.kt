e53752de19a3ac811ff0fe19b004705952fd5ccc
Harish K <kpharish2001@gmail.com>
2026-08-27 12:13:40 +0530

    Create SkybapPlugin.kt

---
+++ Skybap/src/main/kotlin/com/skybap/SkybapPlugin.kt
@@ -0,0 +1,30 @@
+package com.skybap
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class SkyBapPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(SkyBapProvider())
+
+        // Non-core hosts used by SkyBap's download/watch links. Once
+        // registered here, plain loadExtractor() calls anywhere (including
+        // inside SkyBapHowblogs itself) will route to these automatically.
+        registerExtractorAPI(SkyBapHubCloud())
+        registerExtractorAPI(SkyBapVCloud())
+        registerExtractorAPI(SkyBapGDFlix())
+        registerExtractorAPI(SkyBapGDLink())
+        registerExtractorAPI(SkyBapGDFlixApp())
+        registerExtractorAPI(SkyBapGdFlix1())
+        registerExtractorAPI(SkyBapGdFlix2())
+        registerExtractorAPI(SkyBapHubdrive())
+        registerExtractorAPI(SkyBapDriveleech())
+        registerExtractorAPI(SkyBapDriveseed())
+        registerExtractorAPI(SkyBapGofile())
+        registerExtractorAPI(SkyBapHowblogs())
+        registerExtractorAPI(SkyBapTpead())
+        registerExtractorAPI(SkyBapAdvtpe())
+    }
+}
