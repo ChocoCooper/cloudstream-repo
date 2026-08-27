02e38213884376226c6e149e5e9f2c998ad561e4
Harish K <kpharish2001@gmail.com>
2026-08-27 20:43:12 +0530

    Update EpornerPlugin.kt

--- Eporner/src/main/kotlin/com/Eporner/EpornerPlugin.kt
+++ Eporner/src/main/kotlin/com/Eporner/EpornerPlugin.kt
@@ -7,6 +7,6 @@ import android.content.Context
 @CloudstreamPlugin
 class EpornerPlugin: Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(EpornerProvider())
+        registerMainAPI(Eporner())
     }
 }
