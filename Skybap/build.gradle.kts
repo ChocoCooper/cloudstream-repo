6a64bd3bcbd1cb08a7b6102395ffdd61dfdc1b44
Harish K <kpharish2001@gmail.com>
2026-08-29 21:21:18 +0530

    Update build.gradle.kts

--- Skybap/build.gradle.kts
+++ Skybap/build.gradle.kts
@@ -1,10 +1,10 @@
 // use an integer for version numbers
-version = 1
+version = 2
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = ""
+    description = "Skymovieshd Provider"
     language = "hi"
     authors = listOf("ChocoCooper")
 
