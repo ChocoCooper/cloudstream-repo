559c1da3c4cc3d7b377d809a575adb2eacdb107c
Harish K <kpharish2001@gmail.com>
2026-09-02 10:37:53 +0530

    Create Krx18Plugin.kt

---
+++ Krx18/src/main/kotlin/com/Krx18/Krx18Plugin.kt
@@ -0,0 +1,15 @@
+package com.KRX18
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class Krx18Plugin : Plugin() {
+    override fun load(context: Context) {
+        registerProvider(KRX18Provider())
+        registerExtractorAPI(PlayKrx18Extractor())
+        registerExtractorAPI(Mov18plusExtractor())
+        registerExtractorAPI(LoadvidExtractor())
+    }
+}
