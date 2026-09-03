a2be60c539fa91cffa6f7e0d29b5ea878e0ca6cc
Harish K <kpharish2001@gmail.com>
2026-09-03 23:04:16 +0530

    Update Mov18plusExtractor.kt

--- Krx18/src/main/kotlin/com/Krx18/Mov18plusExtractor.kt
+++ Krx18/src/main/kotlin/com/Krx18/Mov18plusExtractor.kt
@@ -2,10 +2,7 @@ package com.KRX18
 
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.utils.ExtractorApi
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.INFER_TYPE
-import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.*
 
 class Mov18plusExtractor : ExtractorApi() {
     override val name = "Mov18plus"
@@ -33,7 +30,7 @@ class Mov18plusExtractor : ExtractorApi() {
         val videoUrl = extractVideo(iframeDoc)
         if (videoUrl != null) {
             callback.invoke(
-                ExtractorLink(
+                newExtractorLink(
                     source = name,
                     name = "$name MP4",
                     url = videoUrl,
