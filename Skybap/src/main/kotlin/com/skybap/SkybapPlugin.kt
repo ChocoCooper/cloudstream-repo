95e8ef4603eafb7fc89ae51f38332b3e5284a361
Harish K <kpharish2001@gmail.com>
2026-09-21 19:05:01 +0530

    Update SkybapPlugin.kt

--- Skybap/src/main/kotlin/com/skybap/SkybapPlugin.kt
+++ Skybap/src/main/kotlin/com/skybap/SkybapPlugin.kt
@@ -1,30 +1,35 @@
 package com.skybap
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
 class SkyBapPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(SkyBapProvider())
 
-        // Non-core hosts used by SkyBap's download/watch links. Once
-        // registered here, plain loadExtractor() calls anywhere (including
-        // inside SkyBapHowblogs itself) will route to these automatically.
-        registerExtractorAPI(SkyBapHubCloud())
-        registerExtractorAPI(SkyBapVCloud())
-        registerExtractorAPI(SkyBapGDFlix())
-        registerExtractorAPI(SkyBapGDLink())
-        registerExtractorAPI(SkyBapGDFlixApp())
-        registerExtractorAPI(SkyBapGdFlix1())
-        registerExtractorAPI(SkyBapGdFlix2())
-        registerExtractorAPI(SkyBapHubdrive())
-        registerExtractorAPI(SkyBapDriveleech())
-        registerExtractorAPI(SkyBapDriveseed())
-        registerExtractorAPI(SkyBapGofile())
-        registerExtractorAPI(SkyBapHowblogs())
-        registerExtractorAPI(SkyBapTpead())
-        registerExtractorAPI(SkyBapAdvtpe())
+        // Direct-CDN extractors solved from live recon.
+        registerExtractorAPI(SkyBapPixeldrain())      // pixeldrain.com / pixeldrain.dev
+        registerExtractorAPI(SkyBapPixelHubcloud())   // pixel.hubcloud.ist  (10 Gbps direct)
+        registerExtractorAPI(SkyBapBusyCdn())         // instant.busycdn.xyz (signed direct)
+        registerExtractorAPI(SkyBapGoflix())          // goflix.sbs  (mirror + /download-fast/…)
+        registerExtractorAPI(SkyBapGamerxyt())        // gamerxyt.com/hubcloud.php generator
+
+        // Panel / file-host extractors.
+        registerExtractorAPI(SkyBapHubCloud())        // https://hubcloud.*  (+ /drive/)
+        registerExtractorAPI(SkyBapVCloud())          // https://vcloud.*
+        registerExtractorAPI(SkyBapGDFlix())          // https://gdflix.*
+        registerExtractorAPI(SkyBapGDLink())          // https://gdlink.*
+        registerExtractorAPI(SkyBapGDFlixApp())       // https://new.gdflix.*
+        registerExtractorAPI(SkyBapGdFlix1())         // https://new1.gdflix.*
+        registerExtractorAPI(SkyBapGdFlix2())         // https://*.gdflix.*  (rotating subdomains)
+        registerExtractorAPI(SkyBapHubdrive())        // https://hubdrive.*
+        registerExtractorAPI(SkyBapDriveleech())      // https://driveleech.*
+        registerExtractorAPI(SkyBapDriveseed())       // https://driveseed.*
+        registerExtractorAPI(SkyBapGofile())          // https://gofile.io
+        registerExtractorAPI(SkyBapHowblogs())        // https://howblogs.*  (+ *.howblogs.*)
+        registerExtractorAPI(SkyBapTpead())           // https://tpead.net
+        registerExtractorAPI(SkyBapAdvtpe())          // https://advtpe.*
     }
 }
